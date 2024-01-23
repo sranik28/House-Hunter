089b721cca3b31ca692c5aa3fb9c85c89ecca061
MD Anik <122194751+sranik28@users.noreply.github.com>
2024-01-23 12:03:03 +0600

    banner added

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from '../components/shared/NavBer';
+import { Outlet } from 'react-router-dom';
 
 const Root = () => {
     return (
         <div>
             <NavBar/>
+            <Outlet/>
         </div>
     );
 };
